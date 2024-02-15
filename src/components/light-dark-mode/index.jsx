170dc3c2abde7ed088665a1abf277cfcdc15e883
anyars-encarta <anyarsencarta@gmail.com>
2024-02-15 07:09:20 +0000

    Install and Correct linter errors

--- src/components/light-dark-mode/index.jsx
+++ src/components/light-dark-mode/index.jsx
@@ -1,22 +1,21 @@
-import useLocalStorage from "./useLocalStorage";
+import useLocalStorage from './useLocalStorage';
 import './styles.css';
 
 const LightDarkMode = () => {
+  const [theme, setTheme] = useLocalStorage('theme', 'dark');
 
-    const [theme, setTheme] = useLocalStorage('theme', 'dark');
+  const handleToggleTheme = () => {
+    setTheme(theme === 'light' ? 'dark' : 'light');
+  };
 
-    const handleToggleTheme = () => {
-      setTheme(theme === 'light' ? 'dark' : 'light')
-    };
-
-    return (
-    <div className='light-dark-mode' data-theme={theme}>
-        <div className='container'>
-          <p>Hello World !</p>
-          <button onClick={handleToggleTheme}>Change Theme</button>
-        </div>
+  return (
+    <div className="light-dark-mode" data-theme={theme}>
+      <div className="container">
+        <p>Hello World !</p>
+        <button type="button" onClick={handleToggleTheme}>Change Theme</button>
+      </div>
     </div>
-    );
+  );
 };
 
 export default LightDarkMode;
